bb82ca7761ff7e5750a7fe66616369ad1a4365c1
chuckpaul98@icloud.com <P.aul98x>
2021-04-02 09:30:50 +0200

    First Commit

---
+++ Project/sketch.js
@@ -0,0 +1,27 @@
+let kangaroo;
+let kanImg;
+let barrImg;
+let bImg;
+
+function preload(){
+  kanImg = loadImage('Project/Images/kangaroo.png');
+  barrImg = loadImage('Project/Images/barrel.png');
+  bImg = loadImage('Project/Images/background.jpg');
+}
+
+function setup() {
+  createCanvas(800, 450);
+  kangaroo = new Kangaroo();
+}
+
+function keyPressed(){
+  if(key == ' '){
+    kangaroo.jump( );
+  }
+}
+
+function draw() {
+  background(bImg);
+  kangaroo.show();
+  kangaroo.move();
+}
