cab65f0bb299245cadd2017b1d7d94af41ce1348
chuckpaul98@icloud.com <P.aul98x>
2021-04-02 09:54:08 +0200

    2 Commit

---
+++ Barrel.js
@@ -0,0 +1,8 @@
+class Barrel{
+  
+  constructor(){
+    this.dim = 100;
+    this.x = width;
+    this.y = height - this.dim;
+  }
+}
---
+++ Kangaroo.js
@@ -0,0 +1,27 @@
+gravity = 2.5;
+
+class Kangaroo {
+  constructor() {
+    this.dim = 100;
+
+    this.x = this.dim / 2;
+    this.y = height - this.dim;
+
+    this.velocityX = 0;
+
+  }
+
+  jump() {
+    this.velocityX = -25;
+  }
+
+  move() {
+    this.y += this.velocityX;
+    this.velocityX += gravity;
+    this.y = constrain(this.y, 0, height - this.dim);
+  }
+
+  show() {
+    rect( this.x, this.y, this.dim, this.dim);
+  }
+}
